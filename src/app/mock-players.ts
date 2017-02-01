69dd9c574b06074e03c2100c520466077bb5c103
Getro Naissance <getro89@gmail.com>
2017-02-01 14:44:43 -0800

    it will add a dependency injection

---
+++ src/app/mock-players.ts
@@ -0,0 +1,8 @@
+import { Player } from './player.model';
+
+export const PLAYERS: Player[] = [
+  new Player("Getro Naissance", 10, "defense", "Usually starts in the second Half", 5),
+  new Player("Getro Naissance", 9, "striker", "Usually starts in the second Half", 3),
+  new Player("Getro Naissance", 7, "offense", "Usually starts in the second Half", 2),
+  new Player("Getro Naissance", 5, "mid-field", "Usually starts in the second Half", 1)
+];
--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Player } from '../player.model';
 import { Router } from '@angular/router';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-roster',
   templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.css']
+  styleUrls: ['./roster.component.css'],
+  providers: [PlayerService]
 })
-export class RosterComponent  {
+export class RosterComponent implements OnInit  {
 
-  constructor(private router: Router){}
+  players: Player[];
 
-  players: Player[] = [
-    new Player("Getro Naissance", 10, "defense", "Usually starts in the second Half", 5),
-    new Player("Getro Naissance", 9, "striker", "Usually starts in the second Half", 3),
-    new Player("Getro Naissance", 7, "offense", "Usually starts in the second Half", 2),
-    new Player("Getro Naissance", 5, "mid-field", "Usually starts in the second Half", 1)
-  ];
+  constructor(private router: Router, private playerService: PlayerService){}
+
+  ngOnInit() {
+    this.players = this.playerService.getPlayers();
+  }
 
   goToDetailPage(clickedAlbum: Player) {
    this.router.navigate(['players', clickedAlbum.id]);
